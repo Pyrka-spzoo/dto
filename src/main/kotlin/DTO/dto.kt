d4d7bec7ea48252fef4ddd002e455d256f97bc43
bogusz <bogusz2005@gmail.com>
2024-06-06 10:39:14 +0200

    before refactor

--- src/main/kotlin/DTO/dto.kt
+++ src/main/kotlin/DTO/dto.kt
@@ -6,10 +6,16 @@ import kotlinx.serialization.json.*
 import kotlinx.serialization.serializer
 import kotlinx.serialization.Serializable
 
-
-interface Jsonable {
-    fun toJson(json: Json = Json): String;
+interface ToJson{
+    fun toJson(json: Json = Json):String
 }
+//return Mess.ErrorMessage(ErrorMessageDTO("ToJson")).toJson()
+
+interface FromJson<out O>{
+    fun fromJson(json: Json = Json): O
+}
+
+interface Jsonable<out O> : ToJson , FromJson<O>
 
 @Serializable
 data class Card(val symbol: String,val color: String)
@@ -35,7 +41,7 @@ data class ErrorMessageDTO(val message: String);
 data class WrongMessage(val message: String);
 
 @Serializable
-sealed class Mess(val message: String) : Jsonable {
+sealed class Mess(val message: String) : Jsonable<Mess> {
 
     @Serializable
     class PayloadMap : Mess{
@@ -65,7 +71,14 @@ sealed class Mess(val message: String) : Jsonable {
     data class ListPlayers(val payload: List<UserDTO>) : Mess("listRooms");
 
 
+    @Serializable
+    data class Rename(val payload: Pair<String,String>) : Mess("rename");
 
+
+
+    override fun fromJson(json: Json): Mess {
+        return Mess.ErrorMessage(ErrorMessageDTO("fromJson TODO"));
+    }
 
     override fun toJson(json: Json): String = json.encodeToString(json.encodeToJsonElement(this).let {
         return@let JsonObject(it.jsonObject.toMutableMap().filter { i -> i.key!= "type" })
